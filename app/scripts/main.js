02030a2b7506939c25532ee45773649228ae912d
Patric <patric.slagveer@gmail.com>
2016-04-05 14:55:28 +0200

    index with header and footer finished mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,19 +4,22 @@
   $(function() {
     $(function() {
       var $carousel = $('.carousel');
+
       $('body').navigation({
         animation: true,
         pause: 'hover'
       });
-      $carousel.carousel({
-        interval: 800000
-      });
-      $carousel.swiperight(function() {
-        $carousel.carousel('prev');
-      });
-      $carousel.swipeleft(function() {
-        $carousel.carousel('next');
-      });
+      if($carousel) {
+        $carousel.carousel({
+          interval: 800000
+        });
+        $carousel.swiperight(function () {
+          $carousel.carousel('prev');
+        });
+        $carousel.swipeleft(function () {
+          $carousel.carousel('next');
+        });
+      }
     });
   });
 }());
