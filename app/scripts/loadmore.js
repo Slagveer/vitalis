bf4de71bc5be497c8bc6f5aa934186d4ebb3d1bd
Patric <patric.slagveer@gmail.com>
2016-04-13 18:26:29 +0200

    loadmore finished (547).

--- app/scripts/loadmore.js
+++ app/scripts/loadmore.js
@@ -28,18 +28,33 @@
         getTeasers();
       });
       $arrow = $(this.element).find('.js-arrow');
-      $teaserboxTemplate = $("#teaserbox-template");
       $teaserboxLocation = $('.js-teaserboxes');
 
       function getTeasers(page) {
         if($loadmore.data('href')) {
           $.when(delayAsync(4000), $.get($loadmore.data('href')), $.ajax({url: $loadmore.data('href')}))
             .then(function(delayData, jsonData, ajaxJsonData) {
-              var source = $teaserboxTemplate.html(),
-                template = Handlebars.compile(source),
-                html = template(ajaxJsonData[0].data);
+              var source,
+                template,
+                teaserbox,
+                html;
 
-              $teaserboxLocation.append(html);
+              if(ajaxJsonData){
+                for(var i=0,l=ajaxJsonData[0].Data.Items.length;i<l;i++) {
+                  teaserbox = ajaxJsonData[0].Data.Items[i];
+                  teaserbox.StartDate = moment(teaserbox.StartDate).format('DD MMM YYYY').toUpperCase();
+                  teaserbox.EndDate = moment(teaserbox.EndDate).format('DD MMM YYYY').toUpperCase();
+                  teaserbox.TeaserType = (teaserbox.TeaserType)? teaserbox.TeaserType.toUpperCase() : '';
+                  teaserbox.Jobhours = (teaserbox.Jobhours)? teaserbox.Jobhours.toUpperCase() : '';
+                  teaserbox.Location = (teaserbox.Location)? teaserbox.Location.toUpperCase() : '';
+                  teaserbox.Country = (teaserbox.Country)? teaserbox.Country.toUpperCase() : '';
+                  $teaserboxTemplate = $('#teaserbox-template-' + teaserbox.TemplateName.toLowerCase());
+                  source = $teaserboxTemplate.html(),
+                  template = Handlebars.compile(source),
+                  html = template(teaserbox);
+                  $teaserboxLocation.append(html);
+                }
+              }
               $arrow.removeClass('loadmore');
             }).fail(function(err) {
               $arrow.removeClass('loadmore');
