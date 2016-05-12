c16186f2146b06cfb1f937669e87f0fa791cfba2
Patric <patric.slagveer@gmail.com>
2016-05-12 16:05:56 +0200

    Update

--- app/scripts/googlemaps.js
+++ app/scripts/googlemaps.js
@@ -58,7 +58,7 @@
           "visibility": "on"
         },
         {
-          "color": "#008095"
+          "color": "#82C032"
         }
       ]
     },
@@ -128,10 +128,14 @@
   }
 
   function onChange(evt) {
+    var countrypage;
+
     evt.preventDefault();
     selectedCountry = evt.target.value;
     if(selectedCountry) {
       if(mapType === 'worldwide') {
+        countrypage = $country.find('.js-option[value="' + selectedCountry + '"]').data('countrypage');
+        $countryForm.attr('action', countrypage);
         $countryForm.submit();
       } else {
         map.setCenter(new google.maps.LatLng(countries[selectedCountry][0], countries[selectedCountry][1]));
