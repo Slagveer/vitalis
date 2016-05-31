ef186894a5248b1a499b128d4464f8ec0d6e8993
Patric <patric.slagveer@gmail.com>
2016-05-31 21:52:20 +0200

    Update

--- app/scripts/navigation.js
+++ app/scripts/navigation.js
@@ -57,7 +57,7 @@
         if($search.data('active')) {
           $navigationSearch.collapse('show');
         }
-        setLogo($(this));
+        //setLogo($(this));
       });
 
       function collapseAll() {
